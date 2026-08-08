0c33af757689df3ab335780fa39082774ae14bca
Agnes4Him <ojuhagnes@gmail.com>
2026-08-08 22:04:48 +0100

    update sg

--- infrastructures/modules/security-group/main.tf
+++ infrastructures/modules/security-group/main.tf
@@ -23,6 +23,20 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
